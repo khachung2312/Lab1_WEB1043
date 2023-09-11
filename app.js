25ca945601a497ec319a437a8a7e0810a2d97903
khachung2004 <hungnkph35189@fpt.edu.vn>
2023-09-11 18:17:19 +0700

    update Lab1

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("xin chao ngay moi");
+
+let a = 10
+// set a value of 10
+console.log(a);
+alert(a)
+
+var n = prompt("Nhap ten cua ban: ");
+//Nhập dữ liệu
+document.write(n);
+//Xuất dữ liệu ra màn hình
